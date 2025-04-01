e1be5c2db9b1b1ae4feb8d7454d4294c65774f70
Roberto Messa <wardog1@gmail.com>
2025-04-01 02:04:35 -0300

    fix: used url field

--- image-store-frontend/src/app/component/image/image.component.ts
+++ image-store-frontend/src/app/component/image/image.component.ts
@@ -2,24 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ImagePipe } from '../../pipe/image.pipe';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-image',
-  imports: [ImagePipe, AsyncPipe, RouterLink],
+  imports: [ImagePipe, AsyncPipe, RouterLink, NgIf],
   templateUrl: './image.component.html',
   styleUrl: './image.component.css'
 })
 export class ImageComponent implements OnInit {
 
-  url: string = environment.apiHost + '/download';
+  url: string = environment.apiHost + '/image-store/download';
   fileName: string = '';
+
+  pane: string = 'MAIN';
+
+  resize() {
+    this.pane = 'RESIZE_MESSAGE';
+  }
 
   ngOnInit(): void {
 
     this.route.paramMap.subscribe((params) => {
 
-      this.fileName = environment.apiHost + '/image-store/download/' + params.get("fileName");
+      this.fileName = `${this.url}/${params.get("fileName")}`;
 
     });
 
